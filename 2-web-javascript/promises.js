11a6a6a4977c979a150a91f3a32383aa59c4c48b
Sergiu Uifalean <sergiu.uifalean@tss-yonder.com>
2019-03-04 19:48:28 +0200

    add web js and requests

---
+++ 2-web-javascript/promises.js
@@ -0,0 +1,12 @@
+/**********************************************************
+ * BizzStream - promises.js
+ * Copyright (c) 2015-2018 Maxedy BV. All rights reserved.
+ * Written By: Uifalean Sergiu
+ **********************************************************/
+
+/* Using promises https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+   Write a sleep function that receives as a parameter the number of milliseconds and use it to print something to the console
+   after 3 seconds.
+ */
+
+
---
+++ 3-http-requests/requests.js
@@ -0,0 +1,11 @@
+/**********************************************************
+ * BizzStream - requests.js
+ * Copyright (c) 2015-2018 Maxedy BV. All rights reserved.
+ * Written By: Uifalean Sergiu
+ **********************************************************/
+
+const https = require('https');
+
+/* 1. Using the https library https://nodejs.org/api/https.html make a request to https://pokeapi.co/ to get all red pokemon.
+   2. After fetching the red pokemon, print all of their names separated by a line break.
+*/
