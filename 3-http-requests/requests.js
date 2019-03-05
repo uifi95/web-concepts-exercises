1d391dcca13b864cedb527b4aed63a272624c993
Sergiu Uifalean <sergiu.uifalean@tss-yonder.com>
2019-03-05 11:23:42 +0200

    remove signature

--- 3-http-requests/requests.js
+++ 3-http-requests/requests.js
@@ -1,9 +1,3 @@
-/**********************************************************
- * BizzStream - requests.js
- * Copyright (c) 2015-2018 Maxedy BV. All rights reserved.
- * Written By: Uifalean Sergiu
- **********************************************************/
-
 const https = require('https');
 
 /* 1. Using the https library https://nodejs.org/api/https.html make a request to https://pokeapi.co/ to get all red pokemon.
